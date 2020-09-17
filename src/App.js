10919d59da084e1e8fd2f2e49ef12a9ea627ae56
kingz10102 <kingz10102@gmail.com>
2020-09-16 20:48:33 -0400

    this is a commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { MenuItem, Select, FormControl } from '@material-ui/core';
 import './App.css';
 
 function App() {
+  const [countries, setCountries] = useState(['']);
+  
+  // code insidewill run once []
+  useEffect(() => {
+    // usinf asyns to send a request
+    const getCountriesData = async () => {
+      fetch("https://disease.sh/v3/covid-19/countries")
+      // get the response then only have json 
+      .then((response) => response.json())
+        .then((data) => {
+          // i just ant contries so this will deconstruct
+          const countries = data.map((country) => ({
+            // getting country then assign it to key name
+            // United States, Canada
+            name: country.country,
+            // this gives abbrev USA, CAN
+            value: country.countryInfo.iso2,
+          }));
+          setCountries(countries);
+        });
+      };
+      // caling async inside useEffect
+    getCountriesData();
+  }, []);
+
+
+
   return (
     <div className="app">
-      <div className="app_heading">
+      <div className="app__heading">
      <h1>COVID19-STATCAST</h1>
      {/* Material UI element  */}
      {/* Creates a form like box for list */}
@@ -15,9 +42,10 @@ function App() {
               variant="outlined"
               value='zyz'
             >
-              <MenuItem value="worldwide">Worldwide</MenuItem>
-              <MenuItem value="worldwide">Country</MenuItem>
-              <MenuItem value="worldwide">Nation</MenuItem>
+              {/* mapping through to get country name and value from useEffect */}
+              {countries.map((country) => (
+                <MenuItem value={country.value}>{country.name}</MenuItem>
+              ))}
             </Select>
           </FormControl>
           </div> 
