3a7d8f58b92ce34045a7614e64abfe8e2000b314
kingz10102 <kingz10102@gmail.com>
2020-09-16 20:19:15 -0400

    this is a commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import { MenuItem, Select, FormControl } from '@material-ui/core';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
      <h1>Hello world</h1>
+     {/* Material UI element  */}
+     {/* Creates a form like box for list */}
+     <FormControl className="app__dropdownbox">
+            {/* Select shows droupdown with different attributes */}
+            <Select
+              variant="outlined"
+              value='zyz'
+            >
+              <MenuItem value="worldwide">Worldwide</MenuItem>
+              <MenuItem value="worldwide">Country</MenuItem>
+              <MenuItem value="worldwide">Nation</MenuItem>
+            </Select>
+          </FormControl>
+     {/* Heading */}
+     {/* COVID19  Title*/}
+
     </div>
   );
 }
